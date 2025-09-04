beb5782a9039938c638bd5b3360b9a3a22c6196e
K. Mahesh Chandran <kmahesh110606@outlook.com>
2025-09-04 22:21:31 +0530

    data

---
+++ data.js
@@ -0,0 +1,117 @@
+export const apps = [
+    {
+        id: "vtop",
+        name: "VTOP",
+        url: "https://vtopcc.vit.ac.in",
+        description: "Academics, Hostels, Leave Requests, Grades and more..."
+    },
+    {
+        id: "colab",
+        name: "vitColab",
+        url: "https://vitcolab945.examly.io/login",
+        description: "Online Integrated Code Development Environment (IDE) by iamneo (formerly examly)"
+    },
+    {
+        id: "vitol",
+        name: "VITOL Coursepage",
+        url: "https://vitolcc1.vit.ac.in/login/index.php",
+        description: "Portal for accessing Quizzes and Materials for Non-Graded Courses"
+    },
+    {
+        id: "lms",
+        name: "LMS",
+        url: "https://lms.vit.ac.in/my/",
+        description: "Assignment submissions and Quizzes (formerly Moodle)"
+    },
+    {
+        id: "vhelp",
+        name: "VHelp",
+        url: "https://vhelp.vit.ac.in/vitcc-help-center/",
+        description: "VIT Chennai's very own helpdesk for solving all your problems."
+    },
+    {
+        id: "ffcs",
+        name: "Registration",
+        url: "https://vtopregcc.vit.ac.in/",
+        description: "FFCS Course and Timetable Selection, Feedback, Hostel Registration."
+    },
+    {
+        id: "events",
+        name: "Events",
+        url: "https://vitchennaievents.com/conf1/",
+        description: "Register for Events, Competitions and more..."
+    },
+    {
+        id: "gpa",
+        name: "GPA Calculator",
+        url: "https://cgpa-calculator.rajchandra.me/",
+        description: "Tool to calculate GPA and CGPA."
+    },
+    {
+        id: "Library",
+        name: "Library",
+        url: "https://library.vit.ac.in/",
+    },
+    {
+        id: "WebCC",
+        name: "WebCC",
+        url: "https://webcc.vit.ac.in/",
+        description: "STS Exam Link"
+    },
+    {
+        id: "facutlyreview",
+        name: "VIT Faculty Review",
+        url: "https://vitfacultyreview.com/",
+        description: "Rate and Review your Professors"
+    },
+    {
+        id: "placements",
+        name: "Placements Tracker", 
+        url: "https://vitplacements.rajchandra.me/",
+        description: "Track Placement Statistics"
+    },
+    {
+        id: "vimegle",
+        name: "ViMeglE",
+        url: "https://vimegle.vit.ac.in/",
+        description: "Chat with Random VITians"
+    },
+    {
+        id: "eventsandrectruitments",
+        name: "Recruitments Portal",
+        url: "https://recruitments.vit.ac.in/",
+        description: "Internship and Job Applications"
+    },
+    {
+        id: "rankpredictor",
+        name: "VIT Rank Predictor",
+        url: "https://vitrankpredictor.rajchandra.me/",
+        description: "Predict your VITEEE Rank"
+    }
+];
+
+export const examschedle = [
+    {
+        id: "fs25cat",
+        name: "Fall Semester 2025-26 CAT Schedule"
+    },
+    {
+        id: "fs25fat",
+        name: "Fall Semester 2025-26 FAT Schedule"
+    }
+];
+
+export const calendar = [
+    {
+        id: "fs25",
+        name: "Fall Semester 2025-26 Academic Calendar"
+    },
+    {
+        id: "fintra25", 
+        name: "Fall Intra-Weekend Semester 2025-26 Academic Calendar"
+    },
+    {
+        id: "ws25",
+        name: "Winter Semester 2025-26 Academic Calendar"
+    }
+];
---
+++ src/AcademicCalendar.jsx
@@ -0,0 +1,19 @@
+// src/pages/AcademicCalendar.jsx
+import { Link } from "react-router-dom";
+import { calendar } from "../data";
+
+export default function AcademicCalendar() {
+  return (
+    <div className="listcontainer">
+      {calendar.map(item => (
+        <Link
+          key={item.id}
+          to={`/academiccalendar/${item.id}`}
+          className="quicklink-item"
+        >
+          <div className="quicklink-item text">{item.name}</div>
+        </Link>
+      ))}
+    </div>
+  );
+}
