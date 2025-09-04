751388ebaa20f07f37d35ff0f0d237d43561c03b
K. Mahesh Chandran <kmahesh110606@outlook.com>
2025-09-04 22:54:12 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import Home from "./pages/Home";
-import AcademicCalendar from "./AcademicCalendar";
-import PdfViewer from "./PDFViewer";
+import { Routes, Route } from 'react-router-dom';
+import AcademicCalendar from './AcademicCalendar';
+import PdfViewer from './PDFViewer';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route path="/academiccalendar" element={<AcademicCalendar />} />
-        <Route path="/academiccalendar/:id" element={<PdfViewer />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<div>Home Page</div>} />
+      <Route path="/calendar" element={<AcademicCalendar />} />
+      <Route path="/calendar/:id" element={<PdfViewer />} />
+      <Route path="/examschedule" element={<AcademicCalendar />} />
+      <Route path="/examschedule/:id" element={<PdfViewer />} />
+      <Route path="/all" element={<div>All Apps Page</div>} />
+      <Route path="/about" element={<div>About Page</div>} />
+      <Route path="/privacy" element={<div>Privacy Policy Page</div>} />
+      <Route path="/terms" element={<div>Terms and Conditions Page</div>} />
+      <Route path="/request" element={<div>Request Page</div>} />
+      <Route path="*" element={<div>404 Not Found</div>} />
+    </Routes>
   );
 }
